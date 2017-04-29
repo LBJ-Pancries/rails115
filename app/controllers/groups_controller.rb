95fdfb8509ba48bb027571105c158a00fc86af5f
liubingjie <breeder1121@hotmail.com>
2017-04-29 20:03:10 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -15,53 +16,39 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.user = current_user
-    # @group.save
-    # flash[:notice] = "New Success"
+
     if @group.save
       redirect_to groups_path notice: "New Success"
     else
       render :new
     end
-    # redirect_to groups_path notice: "New Success"
   end
   # 实作看板的“编辑” edit / update
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-    # @group.update(group_params)
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
       render :edit
     end
-    # flash[:notice] = "Update Success"
-    # redirect_to groups_path, notice: "Update Success"
   end
 
   def destroy
+    @group.destroy
+    redirect_to groups_path, alert: "Group deleted"
+  end
+
+  private
+
+  def find_group_and_check_permission
     @group = Group.find(params[:id])
 
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
     end
-    
-    @group.destroy
-    # flash[:alert] = "Group deleted"
-    redirect_to groups_path, alert: "Group deleted"
   end
-
-  private
 
   def group_params
     params.require(:group).permit(:title, :description)

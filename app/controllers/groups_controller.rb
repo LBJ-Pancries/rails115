e91e2b369d1eca2554aaea8f1153b23a8c17f7d9
liubingjie <breeder1121@hotmail.com>
2017-04-29 19:48:41 +0800

    创建限制条件

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -27,10 +27,18 @@ class GroupsController < ApplicationController
   # 实作看板的“编辑” edit / update
   def edit
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
 
   def update
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
     # @group.update(group_params)
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
@@ -43,6 +51,11 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+    
     @group.destroy
     # flash[:alert] = "Group deleted"
     redirect_to groups_path, alert: "Group deleted"

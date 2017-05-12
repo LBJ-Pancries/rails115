9e786739ffcb56c6ef453994ec4557ea5d9f062e
liubingjie <breeder1121@hotmail.com>
2017-05-12 11:01:33 +0800

    实作图片上传功能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511090957) do
+ActiveRecord::Schema.define(version: 20170512003810) do
 
   create_table "group_relationships", force: :cascade do |t|
     t.integer  "group_id"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170511090957) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "group_id"
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
